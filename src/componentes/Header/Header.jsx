b71e0d971a41d1a843c0c1a2c431871a24df5b3c
edsonjfs <edsonjosefs@gmail.com>
2023-09-27 00:29:07 -0300

    ref: alteracao e centralizacao do botao e texto

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -1,34 +1,87 @@
+import { Link } from 'react-router-dom';
+import './Header.css';
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <div className='containerHeader'>
+      <header className='Header'>
+        <div className='img-container'>
+          <Link to="/index">
+            <img src='/img/comidavddbranco.png' alt='Logo da Header' className='img' />
+          </Link>
+        </div>
+        <div className="menu-container">
+        <nav className="navbar navbar-expand-lg menu">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="/index">
+                  QUEM SOMOS?
+                </a>
+              </li>
+              <li className="nav-item">
+                <Link to="/onde" className="nav-link">
+                  PRESTAÇÃO DE CONTAS
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/colabore" className="nav-link">
+                  COLABORE
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+
+
+
+/*
 import { Link } from 'react-router-dom'
 import './Header.css'
 import React from 'react'
 
-export const Header =() => {
+export const Header = () => {
     return (
         <div className='containerHeader'>
             <header className='Header'>
-            
-                   <div className='img-container'> 
-                   
-                   <Link to="/index"> <a href=""><img src='/img/comidavddbranco.png' alt='Logo da Header' className='img' /></a></Link>
-                   
-                   </div>
-            <div className='menu'>
-            <nav>
-                <ul>
-                    <li>
-                        <a href="/index">QUEM SOMOS?</a>
-                    </li>
-                    <li>
-                        <Link to="/onde" ><a href=''>PRESTAÇÃO DE CONTAS</a> </Link>
-                    </li>
-                    <li>
-                  <Link to="/colabore"> <a href=''>COLABORE</a></Link>
-                    </li>
-            
-                </ul>
-            </nav>
-            </div>
+                <div className='img-container'>
+                    <Link to="/index"> <a href=""><img src='/img/comidavddbranco.png' alt='Logo da Header' className='img' /></a></Link>
+                </div>
+                <div className='menu'>
+                    <nav>
+                        <ul>
+                            <li>
+                                <a href="/index">QUEM SOMOS?</a>
+                            </li>
+                            <li>
+                                <Link to="/onde" ><a href=''>PRESTAÇÃO DE CONTAS</a> </Link>
+                            </li>
+                            <li>
+                                <Link to="/colabore"> <a href=''>COLABORE</a></Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </header>
         </div>
     )
 }
+*/
--- src/componentes/Onde/Onde.jsx
+++ src/componentes/Onde/Onde.jsx
@@ -1,16 +1,33 @@
 import { Header } from "../Header/Header"
 import { Footer } from "../Footer/Footer"
 import './Onde.css'
-import React from 'react' 
+import React from 'react'
 
 
 export const Onde = () => {
-    return(
-     <div>   <Header/>
-    
-<Footer/>
+    return (
+        <div>   <Header />
+            <div className="container">
+            <div className='row'>
+                <div className='col'>
+                    <p> Domingo, 17 de setembro de 2023, foi realizado uma entrega para o trajeto Parque Minas Gerais - Itamaraty. Neste,
+                        percorremos 43 km, em uma atividade de 5 horas e 13 minutos de duração, onde começamos com satisfatória pontualidade as 8h02 e finalizamos o trabalho às 13h15.
+                    </p>
 
-</div>
+                    <p>  74 pessoas, distribuidas em 16 famílias foram atendidas, sendo elas 46 adultos e 28 crianças.
+                         Além disso conseguimos fornecer uma quantidade considerável de doações para o projeto do Robson, da paróquia São Luiz Gonzaga.
+                    </p>
+
+                    <p>
+                        A diversidade de produtos estava alta, contamos com ovos, alface, brócolis, espinafre, couve, batata doce, banana,
+                        mandioca, limão, abóbora, abobrinha, cebolinha, além de sabão liquido que foram adquiridos dos produtores Nalva, Fernando, Yumi, Furnas, Claudemir e Nivaldo
+                    </p>
+                </div>
+            </div>
+            </div>
+            <Footer />
+
+        </div>
     )
 }
 
