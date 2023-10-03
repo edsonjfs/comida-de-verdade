59fab74a1946628f9c3704db2247f8d4171a0d97
edsonjfs <edsonjosefs@gmail.com>
2023-10-02 21:40:57 -0300

    fix: teste add main em app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 
 import AppRoute from './RouteComponent';
+import Main from './componentes/Main/Main';
 
 function App() {
   return (
     <div>
-  <AppRoute/>
+  <Main></Main>
    </div>
   );
 }
